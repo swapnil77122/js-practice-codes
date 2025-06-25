0bf50425f5cf2650da1f08b5d02a40d3638ec3a7
swapnil77122 <swapnildhage7885@gmail.com>
2025-06-25 12:57:45 +0530

    aa

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+//array
+const myArr =[0,1,2,3,4,5,6,7,8]
+console.log(myArr[2]);
+
+
+const myHeors=["shakti","iron"]
+ const myArr2 = new Array(1,2,3,4,5)
+
+ //Arrsy methods
+ myArr.push(5)
+ myArr.push(44)
+ console.log(myArr);
+ myArr.pop()
+ console.log(myArr);
+myArr.unshift(0)
+console.log(myArr);
+myArr.shift()
+console.log(myArr);
+myArr.shift()
+console.log(myArr);
+
+
+// console.log((myArr.includes(3)));
+// console.log(myArr.indexOf(2));
+
+// const newArr=myArr.join()
+// console.log(myArr);
+// console.log(typeof myArr);
+const myn1 =myArr.slice(1,3)
+console.log(myn1);
+console.log("b",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn1);
+
+console.log(myn2);
+
+
+
+ 
